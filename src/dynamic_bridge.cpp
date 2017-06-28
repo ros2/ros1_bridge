97ba6376d5f4f6d1492cf3622ad6c990b8484869
Mikael Arguedas <mikael@osrfoundation.org>
2017-06-27 19:20:39 -0700

    fix style making uncrustify complain

--- src/dynamic_bridge.cpp
+++ src/dynamic_bridge.cpp
@@ -388,7 +388,9 @@ void get_ros1_service_info(
     return;
   }
   ros::TransportTCPPtr transport(new ros::TransportTCP(nullptr, ros::TransportTCP::SYNCHRONOUS));
-  auto transport_exit = rclcpp::make_scope_exit([&transport](){ transport->close(); });
+  auto transport_exit = rclcpp::make_scope_exit([&transport]() {
+    transport->close();
+  });
   if (!transport->connect(host, port)) {
     fprintf(stderr, "Failed to connect to %s:%d\n", host.data(), port);
     return;
