b249ce2b039a3138830defc0a69c87bc50e893c5
potentialdiffer <52542226+potentialdiffer@users.noreply.github.com>
2021-01-21 16:03:54 +0100

    Update ros1_bridge/__init__.py
    Co-authored-by: Chris Lalancette <clalancette@gmail.com>
    Signed-off-by: Spinnler Christian <christian.spinnler@siemens.com>

--- ros1_bridge/__init__.py
+++ ros1_bridge/__init__.py
@@ -818,9 +818,9 @@ def load_ros2_message(ros2_msg):
         message_relative_path += '.msg'
     else:
         raise RuntimeError(
-            "message '%s/msg/%s' " % (ros2_msg.package_name, ros2_msg.message_name) +
-            "was not found in prefix '%s' with either " % (ros2_msg.prefix_path) +
-            "file extension '.msg' or '.idl'")
+            "message '%s/msg/%s' was not found in prefix '%s' with either"
+            "file extension '.msg' or '.idl' %
+            (ros2_msg.package_name, ros2_msg.message_name, ros2_msg.prefix_path))
     # We don't support .msg files, but that shouldn't be a problem since an .idl
     # version should have been created when the package was built by rosidl_adapter.
     if message_path.endswith('.msg'):
